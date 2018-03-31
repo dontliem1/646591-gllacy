d1d429b61f6a7a40a93e5859d26e27d5eab3150c
Michael <dontliem1@gmail.com>
2018-03-31 18:13:02 +0500

    Прикручена интерактивная карта без настроек.

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// var linkmodal = document.querySelector(".js-modal-feedback");
+// var modal = document.querySelector(".modal-feedback");
+// linkmodal.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+// });
