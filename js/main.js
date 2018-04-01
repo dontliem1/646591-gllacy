0bbb316500220996acace5ef444b6c57e05b59e2
Michael <dontliem1@gmail.com>
2018-04-01 05:35:36 +0500

    Собранный сайт с действующими модальным окном, слайдером и интерактивной картой

--- js/main.js
+++ js/main.js
@@ -1,5 +1,64 @@
-// var linkmodal = document.querySelector(".js-modal-feedback");
-// var modal = document.querySelector(".modal-feedback");
-// linkmodal.addEventListener("click", function (evt) {
-//   evt.preventDefault();
-// });
+var link = document.querySelector(".js-modal");
+var modal = document.querySelector(".modal");
+var close = modal.querySelector(".modal-close");
+var form = modal.querySelector("form");
+
+var name = modal.querySelector("[name=name]");
+var mail = modal.querySelector("[name=mail]");
+var question = modal.querySelector("textarea");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("mail");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.add("modal-show");
+  if (storage) {
+    name.value = storage;
+    if (storage) {
+      mail.value = storage;
+      question.focus();
+    } else {
+      mail.focus();
+    }
+  } else {
+    name.focus();
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.remove("modal-show");
+  form.classList.remove("modal-error");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modal.classList.contains("modal-show")) {
+      modal.classList.remove("modal-show");
+      form.classList.remove("modal-error");
+    }
+  }
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!mail.value || !question.value) {
+    evt.preventDefault();
+    form.classList.remove("modal-error");
+    form.offsetWidth = form.offsetWidth;
+    form.classList.add("modal-error");
+    console.log("Введите электронную почту и вопрос.");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", login.value);
+      localStorage.setItem("mail", login.value);
+    }
+  }
+});
